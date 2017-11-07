968cd614bc2e217b0add41cdf0de2743437438fe
kkavitha <kkavitha@thoughtworks.com>
2017-11-07 14:06:42 +0530

    Get locations

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 from find_court_on_date import get_locations
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 
 @app.route("/locations")
 def main():
-    return jsonify(get_locations())
+    locations = get_locations()
+    json = {"locations": locations}
+    return jsonify(json)
 
 
 if __name__ == "__main__":
--- find_court_on_date.py
+++ find_court_on_date.py
@@ -14,9 +14,10 @@ def get_locations():
     r = br.open('http://www.booknplay.in')
     locations = []
     options = r.soup.find_all("select")[1].find_all("option")
-    map(lambda x: locations.append(x["value"]), options)
-    print locations
+    for option in options:
+        locations.append(option["value"])
+    return locations
 
 
-submit_for_date('2017-11-2')
-get_locations()
+# submit_for_date('2017-11-2')
+# get_locations()
