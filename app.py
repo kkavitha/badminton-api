4de61762e42bf96690d850e9ee7d7b89263db570
kkavitha <kkavitha@thoughtworks.com>
2017-11-17 22:39:12 +0530

    Get availability

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify,request
 from flask_cors import CORS
 
+from find_court_on_date import get_availability
 from find_court_on_date import get_locations
 
 app = Flask(__name__)
@@ -13,6 +14,14 @@ def main():
     locations = get_locations()
     json = {"locations": locations}
     return jsonify(json)
+
+
+@app.route("/get-availability", methods=['POST'])
+def get_availabilities():
+    slots = request.json["slots"]
+    areas = request.json["area"]
+    get_availability(areas, slots)
+    return jsonify(availability="")
 
 
 if __name__ == "__main__":
--- find_court_on_date.py
+++ find_court_on_date.py
@@ -1,12 +1,32 @@
+import datetime
+import re
+
 import mechanicalsoup
 
 
-def submit_for_date(date):
-    browser = mechanicalsoup.StatefulBrowser()
-    browser.open('http://www.booknplay.in')
-    browser.select_form('#contact-formm')
-    browser['data[Ground][date]'] = date
-    browser.submit_selected()
+def submit_for_date(date, areas):
+    for area in areas:
+        print(area)
+        browser = mechanicalsoup.StatefulBrowser()
+        browser.open('http://www.booknplay.in')
+        browser.select_form('#contact-formm')
+        browser['data[Ground][date]'] = date
+        browser['data[Ground][area]'] = area
+        page = browser.submit_selected()
+        view_slots = page.soup.find_all("button", class_="view_slot")
+        if len(view_slots) > 0:
+            value = view_slots[0]["value"]
+            url = "http://www.booknplay.in/grounds/booking_layout/" + value + "/" + date + "/1"
+            r = browser.open(url)
+            divs = r.soup.find_all("div", id="court_lists")
+            courts = divs[0].find_all("a", href=re.compile(r'court_lists'))
+            for court in courts:
+                court_id = court["href"][1:]
+                print court_id
+                court_div = r.soup.find_all("div", id=court_id)
+                slots = court_div[0].find_all("td", class_="available single_slot")
+                for slot in slots:
+                    print (slot["value"])
 
 
 def get_locations():
@@ -19,5 +39,18 @@ def get_locations():
     return locations
 
 
+def get_availability(areas, slots):
+    date_formate = '%Y-%m-%d'
+    today = datetime.date.today().strftime(date_formate)
+    tomorrow = (datetime.date.today() + datetime.timedelta(days=1)).strftime(date_formate)
+    day_after_tomorrow = (datetime.date.today() + datetime.timedelta(days=2)).strftime(date_formate)
+    # submit_for_date(str(today))
+    # submit_for_date(str(tomorrow))
+    areas = get_locations()
+    submit_for_date(str(day_after_tomorrow), areas)
+
+
 # submit_for_date('2017-11-2')
 # get_locations()
+
+
